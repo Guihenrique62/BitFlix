74cd40a563e512215fe334b75178d73ccdacddfd
Guilherme Henrique <guilhermeportosantos1@gmail.com>
2024-04-18 22:42:50 -0300

    CREATE Services and Helpers for GET Categories

--- src/controllers/categoriesController.ts
+++ src/controllers/categoriesController.ts
@@ -1,18 +1,18 @@
-
-
 import { Request, Response } from 'express'
-import { Category } from '../models'
+import { categoryService } from '../services/categoriesService'
+import { getPaginationParams } from '../helpers/getPaginationParams'
 
 export const categoriesController = {
   index: async (req: Request, res: Response) => {
 
-    try {
-        const categories = await Category.findAll({
-            order: [['position', 'ASC']]
-        })
-        return res.json(categories)
+	const [page, perPage] = getPaginationParams(req.query)
 
+    try {
+        const paginatedCategories = await categoryService.findAllPaginated(page,perPage)
+
+        return res.status(201).json(paginatedCategories)
     } catch (err) {
+
         if(err instanceof Error){
             return res.status(500).json({message: err.message})
         }
